dbcca25eba37dbf8bb280425b491721ce7627d96
mtlewis1291 <mtlewis1291@gmail.com>
2018-04-18 13:21:19 -0500

    Add files via upload

---
+++ DifficultySelect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultySelect : MonoBehaviour {
+	
+	public DifficultySelect(){
+		NumberWizard.GuessAttempts = 6;
+	}
+
+	public void SetDifficulty(string difficulty) {
+		switch (difficulty) {
+			case "Hard":
+				NumberWizard.GuessAttempts = 7;
+				break;
+			case "Normal":
+				NumberWizard.GuessAttempts = 6;
+				break;
+		}
+	}
+}
---
+++ NumberWizard.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NumberWizard : MonoBehaviour {
+
+    int max;
+    int min;
+    int guess;
+	int counter;
+
+	public static int GuessAttempts { get; set; }
+	public Text guessText;
+	public Text attemptsText;
+
+    void Start() {
+        StartGame();
+	}
+
+    void StartGame() {
+        max = 101;
+        min = 1;
+		counter = GuessAttempts - 1;
+		attemptsText.text = "Guesses remaining: " + counter.ToString();
+		guess = Random.Range(10, 90);
+		guessText.text = guess.ToString();
+    }
+
+	public void GuessLower() {
+		if (max - min > 1){
+			max = guess;
+			NextGuess();
+			GuessCounter();
+		}
+		else if (max - min <= 1){
+			SceneManager.LoadScene("Liar");
+			Debug.Log("Level load requested for: Liar");
+		}
+	}
+
+	public void GuessHigher() {
+		if (max - min > 1){
+			min = guess + 1;
+			NextGuess();
+			GuessCounter();
+		}
+		else if (max - min <= 1){
+			SceneManager.LoadScene("Liar");
+			Debug.Log("Level load requested for: Liar");
+			}
+	}
+
+	public void GuessCounter() {
+		if (GuessAttempts <= 1) {
+			attemptsText.text = "Final answer";
+		}
+		else {
+			attemptsText.text = "Guesses remaining: " + counter.ToString();
+		}
+	}
+
+    void NextGuess() {
+		if (GuessAttempts % 2 == 0){
+			guess = (min + max) / 2;
+		}
+		else {
+			guess = Random.Range(min + ((max - min) / 4), max - ((max - min) / 4));
+		}
+
+		GuessAttempts = GuessAttempts - 1;
+		counter = GuessAttempts - 1;
+		if(GuessAttempts <= 0){
+			SceneManager.LoadScene("Win");
+			Debug.Log("Level load requested for: Win");
+		}
+		guessText.text = guess.ToString();
+    }
+}
